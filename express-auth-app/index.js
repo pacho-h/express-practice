42d6d1ac9c85ad6b25b1be60027ac1ecfdab724e
pacho-h <pacho.h90@gmail.com>
2024-10-08 00:09:58 +0900

    auth middleware 작성

--- express-auth-app/index.js
+++ express-auth-app/index.js
@@ -1,7 +1,19 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 
-const express = require('express');
-const jwt = require('jsonwebtoken');
+import express from 'express';
+import jwt from 'jsonwebtoken';
+
+const posts = [
+    {
+        username: 'user1',
+        title: 'Post 1'
+    },
+    {
+        username: 'user2',
+        title: 'Post 2'
+    }
+];
 
 const app = express();
 
@@ -14,6 +26,22 @@ app.post('/login', (req, res) => {
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
     res.json({accessToken: accessToken});
 });
+
+app.get('/posts', authMiddleware, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name));
+});
+
+function authMiddleware(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
 
 const port = 4000;
 app.listen(port, () => {
